aa595e34993af330cca334c5a5f675644464724d
Jesse <lbarbosa.jessica@gmail.com>
2021-08-15 22:15:10 -0300

    code

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:calculadora/calculadora.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculadoraApp());
+}
+
+class CalculadoraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculadora',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Calculadora(),
+    );
+  }
+}
